7d07012f0469b6f1ad0ad14f747a375e4ccc3e71
Mounir Ybanez <rinuom91@gmail.com>
2014-12-01 11:13:16 +0800

    Added a category on UIView for the sample of animation to be done.

---
+++ AnimationSample/UIView+LearningAnimation.h
@@ -0,0 +1,13 @@
+//
+//  UIView+LearningAnimation.h
+//  AnimationSample
+//
+//  Created by mownier on 12/1/14.
+//  Copyright (c) 2014 mownier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (LearningAnimation)
+
+@end
---
+++ AnimationSample/UIView+LearningAnimation.m
@@ -0,0 +1,13 @@
+//
+//  UIView+LearningAnimation.m
+//  AnimationSample
+//
+//  Created by mownier on 12/1/14.
+//  Copyright (c) 2014 mownier. All rights reserved.
+//
+
+#import "UIView+LearningAnimation.h"
+
+@implementation UIView (LearningAnimation)
+
+@end
